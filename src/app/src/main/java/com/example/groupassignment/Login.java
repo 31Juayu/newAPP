a11dc12803f8f25a99f9dd5525887cf64510f943
Tianyi Xu <tx1g20@soton.ac.uk>
2024-04-18 12:19:03 +1000

    Commit of the Login.java, need to finish the functions

--- src/app/src/main/java/com/example/groupassignment/Login.java
+++ src/app/src/main/java/com/example/groupassignment/Login.java
@@ -3,6 +3,9 @@ package com.example.groupassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // find all elements
+        EditText userNameText = (EditText) findViewById(R.id.UserNameEdit);
+        EditText passWordText = (EditText) findViewById(R.id.PassWordEdit);
+        Button loginButton = (Button) findViewById(R.id.Login_button);
+        Button signUpButton = (Button) findViewById(R.id.Sign_up_button);
+        // add the listener
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performLogin();
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performSignUp();
+            }
+        });
+    }
+    private void performLogin(){
+
+    }
+    private void performSignUp(){
+
     }
 }
