45aff68b98605b156c3418e34295f8914a95d468
Tianyi Xu <tx1g20@soton.ac.uk>
2024-04-18 20:01:49 +1000

    Commit of the Login.java, need to finish the functions skeleton

--- src/app/src/main/java/com/example/groupassignment/Login.java
+++ src/app/src/main/java/com/example/groupassignment/Login.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    private EditText userNameText;
+    private EditText passWordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // find all elements
-        EditText userNameText = (EditText) findViewById(R.id.UserNameEdit);
-        EditText passWordText = (EditText) findViewById(R.id.PassWordEdit);
+        userNameText = (EditText) findViewById(R.id.UserNameEdit);
+        passWordText = (EditText) findViewById(R.id.PassWordEdit);
         Button loginButton = (Button) findViewById(R.id.Login_button);
         Button signUpButton = (Button) findViewById(R.id.Sign_up_button);
         // add the listener
@@ -33,7 +36,11 @@ public class Login extends AppCompatActivity {
         });
     }
     private void performLogin(){
+        String userName = userNameText.getText().toString().trim();
+        String passWord = passWordText.getText().toString().trim();
+        if(userName.isEmpty() || passWord.isEmpty()){
 
+        }
     }
     private void performSignUp(){
 
