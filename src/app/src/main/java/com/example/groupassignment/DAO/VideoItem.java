36449ba5a3d0caba8e8524911a2be92ef05fa353
Tianyi Xu <u7780366@anu.edu.au>
2024-05-15 23:55:29 +1000

    Finish the reference and comments

--- src/app/src/main/java/com/example/groupassignment/DAO/VideoItem.java
+++ src/app/src/main/java/com/example/groupassignment/DAO/VideoItem.java
@@ -1,18 +1,39 @@
 package com.example.groupassignment.DAO;
 
+/**
+ * @author Tianyi Xu
+ * Represents an item for a course which is a video.
+ * This class stores information about a video file, including its name and URI location.
+ */
 public class VideoItem {
+    // name of a video
     String videoName;
+    // uri of a video
     String videoUri;
 
+    /**
+     * The constructor of the video item
+     * This constructor initializes the videoName and videoUri fields of the object.
+     * @param videoName
+     * @param videoUri
+     */
     public VideoItem(String videoName, String videoUri) {
         this.videoName = videoName;
         this.videoUri = videoUri;
     }
 
+    /**
+     * get the video name
+     * @return
+     */
     public String getVideoName() {
         return videoName;
     }
 
+    /**
+     * get the video uri
+     * @return
+     */
     public String getVideoUri() {
         return videoUri;
     }
